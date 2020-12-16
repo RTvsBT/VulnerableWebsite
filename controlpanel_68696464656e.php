2c9a872610d227a889ac4fcab2fab82a16779ad7
wotwot <r.vanvliet@student.fontys.nl>
2020-12-16 02:20:54 +0100

    Update controlpanel_68696464656e.php

--- controlpanel_68696464656e.php
+++ controlpanel_68696464656e.php
@@ -3,7 +3,7 @@ require_once 'common.php';
 $config = parse_ini_file('config.ini', true);
 
 
-if(isset($_POST['api']) && $_POST['api'] == $config['main']['api'])
+if(isset($_POST['API']) && $_POST['API'] == $config['main']['api'])
 {
      if(isset($_POST['ddos'])){
       if ($config['main]['ddos'] == "off"){
